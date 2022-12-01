bd9f2a25a44c08e4cf7e341ade6eac4e2eb740a1
Topher Chung <87874753+wf-tchung@users.noreply.github.com>
2022-12-01 08:43:09 -0800

    cleaned up code, performance inprovements

--- 2022/day01/index.js
+++ 2022/day01/index.js
@@ -7,53 +7,33 @@ let filename = `${day}.in`
 //filename = `${day}.in`
 let input = importFile(dir, filename).split('\n').map(x => parseInt(x, 10))
 //let input = importFile(dir, filename).split('\n')
+let elfCal = [];
 
 
 console.clear()
 
 const part1 = () => {
-    let elfCal = [];
+
+    let topElf = 0;
+
     let sum = 0;
     let elfCount=0;
     for(let i = 0; i < input.length; i++) {
         if(isNaN(input[i])){
             elfCal.push(sum);
+            if (topElf < sum ) topElf = sum;
             sum=0;
             continue;
         }
         sum += input[i];
     }
-
-    let topElf = -9999999999;
-    for (let j=0; j<elfCal.length;j++){
-        if (topElf < elfCal[j]){
-            topElf = elfCal[j];
-        }
-    }
-
     console.log("Elf with most calories: " + topElf)
-    return sum
 }
 
 const part2 = () => {
-    let elfCal = [];
-    let sum = 0;
-    let elfCount=0;
-    for(let i = 0; i < input.length; i++) {
-        if(isNaN(input[i])){
-            elfCal.push(sum);
-            sum=0;
-            continue;
-        }
-        sum += input[i];
-    }
-
     elfCal = elfCal.sort()
-
     let top3Cal = elfCal[elfCal.length-1] + elfCal[elfCal.length-2] + elfCal[elfCal.length-3];
-
     console.log("Top 3 Elf calories: " + top3Cal)
-    return sum
 }
 
 console.log(`Advent of Code 2022 - Day ` + day)
@@ -64,6 +44,7 @@ const p1end = performance.now()
 
 console.log(`part1: ${(p1end - p1start).toFixed(3)}ms`)
 console.log('part1', p1)
+
 
 const p2start = performance.now()
 const p2 = part2()
---
+++ 2022/day01/readme.md
@@ -0,0 +1,3 @@
+## JavaScript
+[![Part 1](https://img.shields.io/badge/Part%201-0.297ms-informational)](https://adventofcode.com/2022/)
+[![Part 2](https://img.shields.io/badge/Part%202-0.156ms-informational)](https://adventofcode.com/2022/)
